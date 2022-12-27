fbfb3811f6c371792b1d1be72078cd7e50477deb
rdush <rdush824@gmail.com>
2022-12-27 21:34:08 +0200

    add css

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,9 @@
 import { MutatingDots } from 'react-loader-spinner';
+import styles from './Loader.module.css';
 
 export const Loader = () => {
   return (
-    <div>
+    <div className={styles.loader}>
       <MutatingDots 
   height="100"
   width="100"
